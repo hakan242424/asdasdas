1cf07855fea44114d14b675cd0497ed4ef78ac32
hakan242424 <71662601+hakan242424@users.noreply.github.com>
2020-09-21 20:35:03 +0300

    Add files via upload

---
+++ komutlar/komut.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+exports.run = async(client, message, args) => {
+
+if (!message.author.hasPermission("KULLANA BILECEK YETKI İSMİ")) return message.channel.send("⚠ Bu Komutu Kullana Bilmek için `KULLANA BILECEK YETKI İSMİ` Yetkisine Sahip Olmalısın!")
+
+const embed = new Discord.RichEmbed()
+
+.setColor("MESAJ RENGI INGILIZCE YAZIN") //RANDOM yazarsanız rastgele renk atar
+.setTitle("MESAJ BAŞLIĞINIZ")
+.setTimestamp() //Bu Ek Saati'de Yollamaya Yarar
+.setDescription("MESAJINIZ | ACIKLAMANIZ")
+.setFooter("ALT SATIR BASLIGINIZ")
+.setImage("Fotoğraf yada Gifiniz Büyük Gözüksün İstiyorsanız Buraya Linkini Koyun")
+.setThumbnail("Fotoğraf yada Gifiniz Sağ Üstte Küçük Gözüksün İstiyorsanız Buraya Linkini Koyun")
+
+message.channel.send(embed)
+
+}
+
+exports.conf = {
+enabled: true, //Komutun Aktif Olup Olmamasını Ayarlarsınız! true: Aktif | false: Kapalı
+guildOnly: false, //Komutun Sunucu Dışında Aktif Olup Olmamasını Ayarlarsınız! true: Aktif | false: Kapalı
+aliases: [], //Ekstra Komut Anahtarları Gire Bilirsiniz! ["Anahtar-ismi"] Gibi
+permLevel: 0 //Kullanım Seviyelerini Ayarlarsınız 0 Herkes Kullana Bilir Demektir!
+};
+
+exports.help = {
+name: 'Komutunuzun Kullanım Anahtarı İsmi',
+description: 'Komutun Açıklaması',
+usage: 'Komutun Kullanım Şekli'
+}
+
+
+//erdem 15
+//erdem 15
+//erdem 15
+//erdem 15
+//bunurdaki yazıları kopyalayıp başka komutlar yapabilirsin
